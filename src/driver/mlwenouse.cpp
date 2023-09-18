ac9ed637f1b800da29f5200292559baa8f3116e9
LuanBu <abandtian@gmail.com>
2023-09-18 16:31:58 -0500

    update

---
+++ src/driver/mlwenouse.cpp
@@ -0,0 +1,45 @@
+#include "mlweno.h"
+
+using namespace MLWENO;
+
+void MLWENOUse::AddMLWENOInstance(const unordered_set<std::string>& selectLevels,
+                                  MLWENOPrepare * mlpPtr){
+
+    multiLevelReconstruction * mlrins = new multiLevelreconstruction(); 
+
+    mlrins->SelectWenoReconstLevel(selectLevels, (*mlpPtr));
+
+    mlrIns.push_back(mlrins); 
+
+}
+
+int AssignInstance(const indice& globalCell){
+
+    // No special treatment on boundary as default 
+
+    return 0;
+
+    // ==== template ========================
+    // These boundaryx function can be defined as inline functions in this file.
+//    if (boundary1(globalCell)) {
+//        return 0;
+//    } else if (boundary2(globalCell)){
+//        return 1;
+//    } else {
+//        return 2;
+//    }
+
+}
+
+double Evaluate(const vertex& point,
+                const indice& globalCell){
+
+    double work = 0.0;
+
+    for (){
+
+
+    }
+
+    return work;
+}
---
+++ src/driver/mlwenouse.h
@@ -0,0 +1,62 @@
+#ifndef MLWENOUSE_H_
+#define MLWENOUSE_H_
+
+#include "reconstruction.h"
+#include "util.h"
+
+/**!
+ * An interface created to simplify usage of reconstruction.
+ * Using MLWENO reconstruction should always be calling MLWENOUse class.
+ * Instead of calling multiLevelReconstruction directly.
+ */
+
+namespace MLWENO{
+
+    class MLWENOUse {
+        public:
+             //! A constructor
+             /**! 
+              * Construct a MLWENOUse class. 
+              * This MLWENO containing multiple multi-level weno reconstruction instances.
+              * Construct a MLWENOUse class by assigning a meshinfo pointer to it.
+              */
+
+             MLWENOUse(MeshInfo * mi) {mi_ = mi;};
+
+             /**! A destructor
+              * Destruct a MLWENOUse class.
+              */
+             
+             ~MLWENOUse() {};
+ 
+             /**
+              * Create MLWENO reconstruction instance from a given MLWENOPrepare class.
+              * We prepare for weno reconstruction only once.
+              * We can have different MLWENO class, but only one WENOPrepare.
+              * Create an MLWENO instance with given levels.
+              */
+             void AddMLWENOInstance(const unordered_set<std::string>& selectLevels,
+                                    MLWENOPrepare * mlpPtr);
+
+             /**
+              * User-defined function.
+              * Use this function to treat boundary differently.
+              */
+             int AssignInstance(const indice& globalCell);
+
+             /**
+              * Evaluate a reconstruction value using defined MLWENO instances.
+              */
+             double Evaluate(const vertex& point, 
+                             const indice& globalCell);
+
+        private:
+
+             vector<multiLevelReconstruction * mlrPtr> mlrIns_;
+
+             MeshInfo * mi_;
+    };
+
+}
+
+#endif
