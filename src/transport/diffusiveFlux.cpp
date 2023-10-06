2669ebcfedecd321c9b1d8793367d000bc5a4225
LuanBu <abandtian@gmail.com>
2023-10-06 16:41:48 -0500

    update

--- src/transport/diffusiveFlux.cpp
+++ src/transport/diffusiveFlux.cpp
@@ -8,10 +8,23 @@ inline std::vector<double> evaluateDiffusionFcns(const vector<double>& uR){
 
     vector<double> work(samplePoints.size(), 0);
 
-    for (auto const& p: samplePoints){
-    
-
+    for (int it = 0; it<work.size(); it++){
+        work.at(it) = diffFunc(uR.at(it));   
     }
+
+    return work;
+}
+
+inline double getAdvFluxEdge(){
+
+    double work;
+
+    return work;
+}
+
+double getDiffusiveFluxInterior(const MLWENOUse& mlu){
+
+    double work = 0.0;
 
     return work;
 }
