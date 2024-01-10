45b5dda49d3ccc269b479647bf48762390820e30
t <abandtian@gmail.com>
2024-01-10 09:43:15 -0600

    update

--- src/mfem/error.cpp
+++ src/mfem/error.cpp
@@ -1,23 +1,34 @@
 #include "error.h"
 
-PetscErrorCode L2ErrorElem(const vector<double>& coeff, 
-                           const bndryVal& bndryval, 
+double L2ErrorElemInterior(const vector<double>& weight, 
                            const indice& globalElemIndic,
-                           array<double,3> (*func)(const vertex& point),
+                           std::array<double,3> (*func)(const vertex& point),
                            const valarray<double>& gwf,
                            const vector<vertex>& gpf,
                            basis& basis_,
                            Hdivmixed& hdiv_){
 
-    // Calculate the L2 Error on the given element 
+    // Calculate the L2 Error on the given interior element 
 
     for (int g=0; g<gwf.size(); g++){
         //Loop through gauess quadrature points
         vertex mapped = GaussMapPointsFace(gpg[g], basis_.corners());
         double jac = abs(gaussJacobian(gpf[g], basis_.corners()));
-        double gw = gwf[g]
-        elemError += gw*jac*;
+        double gw = gwf[g];
+        // Evaluate all eight basis functions for the target element
+		  std::array<vertex, 8> hdivwork = hdiv_.ComputeHdivmixed(basis_, mapped);
+        // Combine these values with weights (calculated solution)
+        valarray<double> approxVal = {0.0,0.0};
+        for (int i=0; i<8; i++){
+            approxVal += weight[i]*hdivwork[i]; 
+        }
+        // Get exact values
+		  std::array<double,3> trueSol = func(mapped);
+
+        valarray<double> diff = approxVal - trueSol;
+
+        elemError += gw*jac*(diff[0]*diff[0] + diff[1]*diff[1]);
     }
 
-    return PETSC_SUCCESS;
+    return pow(elemError,0.5);
 }
--- src/mfem/error.h
+++ src/mfem/error.h
@@ -3,14 +3,15 @@
 
 // Return error measured in energy norm or any arbitrary norm
 
-PetscErrorCode L2ErrorElem(const vector<double>& coeff,
-                           const bndryVal& bndryval,
-                           const indice& globalElemIndic,
-                           double (*func)(const vertex& point, 
-                                          const vector<double>& param),
-                           const valarray<double>& gwf,
-                           const vector<vertex>& gpf,
-                           basis& basis_,
-                           Hdivmixed& hdiv_);
+PetscErrorCode L2ErrorElemInterior(const vector<double>& coeff,
+                                   const indice& globalElemIndic,
+                                   double (*func)(const vertex& point, 
+                                                  const vector<double>& param),
+                                   const valarray<double>& gwf,
+                                   const vector<vertex>& gpf,
+                                   basis& basis_,
+                                   Hdivmixed& hdiv_);
+
+PetscErrorCode L2ErrorElemBndry();
 
 #endif
