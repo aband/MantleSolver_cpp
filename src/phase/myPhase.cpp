4544027173e4f2789fc7ebe83b3291d7444dcf89
t <abandtian@gmail.com>
2024-07-24 01:24:45 -0500

    update

--- src/phase/myPhase.cpp
+++ src/phase/myPhase.cpp
@@ -46,12 +46,22 @@ phaseState::phaseState(){
     HD3l_ = rho_.fluid/rho_.solid1 / ste_;
 }
 
+double 
+
 int phaseState::EvalPhase(double CD, double HD){
 
     // Evaluate current phase state with given information 
     int state = 0; 
 
-    if (CD < DBL_EPSILON && ){
+    if (CD < 2*DBL_EPSILON && HD < HD2s_){
+        // Single phase sub-solidus region
+        state = 1;    
+    } else if (CD > 2*DBL_EPSILON && CD < CD3s_ && HD < 0){
+        // Two phase sub-solidus region no liquid
+        state = 2;
+    } else if ((CD > 2*DBL_EPSILON && CD < CD3l_ && 
+                HD > 2*DBL_EPSILON && HD < CD/Xe_/ste_) || 
+               (CD > CD3l_ && CD < CD3s_)){
 
 
     }
@@ -70,36 +80,13 @@ inline double getSysD(Phase * phase, Phi * phi){
     return sysD;
 }
 
+int phaseState::EvalVolumeFrac(int state){
 
+    switch(){
+        case 1:
 
-int SetEutecticPhase(Phase * phase){
+        break;
+    }
 
-    phase->DT = phase->T1-phase->Te;
+} 
 
-    phase->rho_ice = 3e3;
-    phase->rho_sal = 3e3;
-    phase->rho_bri = 3e3;
-
-    phase->rho_bi  = phase->rho_bri/ phase->rho_ice;
-    phase->rho_si  = phase->rho_sal/ phase->rho_ice;
-
-    phase->cp_ice = 1200;
-    phase->cp_sal = 1200;
-    phase->cp_bri = 1200;
-
-    phase->cp_bi  = phase->cp_bri/ phase->cp_ice;
-    phase->cp_si  = phase->cp_sal/ phase->cp_ice;
-
-    phase->Ste = phase->cp_ice*phase->DT/phase->L; 
-
-    // Constant thermal conductivity right now
-    phase->kappa_ice = 5.2;
-    phase->kappa_sal = 4.7;
-    phase->kappa_bri = 4.7;
-
-    phase->kappa_ii = phase->kappa_ice/phase->kappa_ice;
-    phase->kappa_si = phase->kappa_sal/phase->kappa_ice;
-    phase->kappa_bi = phase->kappa_bri/phase->kappa_ice;
-
-    return 0;
-}
