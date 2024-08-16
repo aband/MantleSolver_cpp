c556e60765b34ff1684b947558f3473653d8fcd1
t <abandtian@gmail.com>
2024-08-16 10:02:21 -0500

    update

--- doc/doc/newwritting/plot_eutectic.m
+++ doc/doc/newwritting/plot_eutectic.m
@@ -18,7 +18,7 @@ figure
 plot([0 Xe]*100, [T1, Te], 'k-'), hold on
 plot([Xe 1]*100, [Te, T2], 'k-')
 plot([0  1]*100, [Te, Te], 'k-')
-plot([0  0]*100, [-0.03, -0.03], 'k-')
+plot([0  Xe]*100, [-0.2, -0.2], 'k-')
 plot([0 0 Xe 1 1]*100, [Te T1 Te T2 Te], 'o', 'markerfacecolor', 'w', 'markeredgecolor', 'r')
 text(.9, Te+0.04,'1','fontsize',12,'color','r')
 text(Xe*100+0.2, Te+0.05,'3','fontsize',12,'color','r')
@@ -38,7 +38,7 @@ ylabel 'Dimensionless Temp'
 % bulk composition Temperature plot switch to left side of eutectic
 figure
 plot([0 Xe]/Xe, [T1, Te], 'k-'), hold on
-plot([0  0]/Xe, [-0.03, -0.03], 'k-')
+plot([0  Xe]/Xe, [-0.2, -0.2], 'k-')
 plot([0 0 Xe]/Xe, [Te T1 Te], 'o', 'markerfacecolor', 'w', 'markeredgecolor', 'r')
 plot([0  Xe]/Xe, [Te, Te], 'k-')
 text(0.01, Te+0.04,'1','fontsize',12,'color','r')
@@ -55,4 +55,24 @@ axis square
 xlabel 'Bulk composition'
 ylabel 'Dimensionless Temp'
 
+% H-X plot 
+L = 0.6
 
+figure
+plot([0,Xe]/Xe,[T1+L,Te+L],'k-'), hold on
+plot([0,Xe]/Xe,[Te,Te+L],'k-')
+plot([0,Xe]/Xe, [-0.2, -0.2], 'k-')
+plot([0,Xe]/Xe,[Te,Te],'k-')
+text(0.5,-0.1,'II','fontsize',12,'color','b');
+text(0.3,0.6,'IV','fontsize',12,'color','b');
+text(0.8,0.3,'III','fontsize',12,'color','b');
+text(0.6,1.3,'VI','fontsize',12,'color','b');
+hold off
+
+axis square
+xlabel 'Bulk composition'
+ylabel 'Dimensionless enthalpy'
+
+% Needed HC plot
+figure
+plot()
