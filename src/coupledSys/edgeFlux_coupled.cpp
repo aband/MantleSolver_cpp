1263036679eab2f9da1bafac82dc675c360b35ff
LuanBu <abandtian@gmail.com>
2024-11-01 15:59:05 -0500

    update

--- src/coupledSys/edgeFlux_coupled.cpp
+++ src/coupledSys/edgeFlux_coupled.cpp
@@ -32,20 +32,22 @@ inline indice PickCellInside(const MeshInfo& mi,
 
 }
 
-double edgeFlux(const MeshInfo& mi,
-                const MLWENO::MLWENOUse& mluIn,
-                const MLWENO::MLWENOUse& mluOut,
-                const indice& globalCellIn,
-                const indice& globalCellOut,
-                const std::string& locationIn,
-                const std::string& locationOut,
-                const std::vector<vertex>& edge,
-                const std::vector<vertex>& gauss_p,
-                const std::vector<vertex>& velocity,
-                const std::valarray<double>& gwe,
-                const vetor<double>& param,
-                fluxFunc       fluxfunc,
-                fluxFuncBndry  fluxfuncbndry){
+inline double edgeFlux(const MeshInfo& mi,
+                       const MLWENO::MLWENOUse& mluIn,
+                       const MLWENO::MLWENOUse& mluOut,
+                       const indice& globalCellIn,
+                       const indice& globalCellOut,
+                       const std::string& locationIn,
+                       const std::string& locationOut,
+                       const std::vector<vertex>& edge,
+                       const std::vector<vertex>& gauss_p,
+                       const std::vector<vertex>& velocity,
+                       const std::valarray<double>& gwe,
+                       const vetor<double>& param,
+                       fluxFunc       fluxfuncAdv,
+                       fluxFuncBndry  fluxfuncbndryAdv,
+                       fluxFunc       fluxfuncDif,
+                       fluxFuncBndry  fluxfuncbndryDif){
     // Compute integral of flux on edge
     // mluIn used for value where unit normal points to
     // mluOut used for value where unit normal tails at
@@ -65,17 +67,25 @@ double edgeFlux(const MeshInfo& mi,
 
     // Distinguish different boundary conditions
     if(OutBndryCell(mi, globalCellIn)){
-        fluxpoint = fluxfuncbndry(mi, mlu, edge, unitNormal, len, globalCellOut,
-                                  locationOut, param, AssignBoundary(globalCellOut));
+        fluxpoint = fluxfuncbndryAdv(mi, mlu, edge, unitNormal, len, globalCellOut,
+                                     locationOut, param, AssignBoundary(globalCellOut)) +
+                    fluxfuncbndryDif(mi, mlu, edge, unitNormal, len, globalCellOut,
+                                     locationOut, param, AssignBoundary(globalCellOut)) ;
 
     }else if (OutBndryCell(mi, globalCellIn)){
-        fluxpoint = fluxfuncbndry(mi, mlu, edge, unitNormal, len, globalCellIn,
-                                  locationIn, param, AssignBoundary(globalCellIn));
+        fluxpoint = fluxfuncbndryAdv(mi, mlu, edge, unitNormal, len, globalCellIn,
+                                     locationIn, param, AssignBoundary(globalCellIn)) + 
+                    fluxfuncbndryDif(mi, mlu, edge, unitNormal, len, globalCellIn,
+                                     locationIn, param, AssignBoundary(globalCellIn)) ;
+
     } else {
         // Interior edge
-        fluxpoint = fluxfunc(mi, mluIn, mluOut, edge, unitNormal, 
-                             len, globalCellIn, globalCellOut,
-                             locationIn, locationOut, gauss_p, gpe, param);
+        fluxpoint = fluxfuncAdv(mi, mluIn, mluOut, edge, unitNormal, 
+                                len, globalCellIn, globalCellOut,
+                                locationIn, locationOut, gauss_p, gpe, param) + 
+                    fluxfuncDif(mi, mluIn, mluOut, edge, unitNormal, 
+                                len, globalCellIn, globalCellOut,
+                                locationIn, locationOut, gauss_p, gpe, param) ;
     }
 
     // Integrate with gauess quadratrue scheme
@@ -156,12 +166,13 @@ double * edgeFluxAll(const MeshInfo* mi,
             double len = length(edge);
             vertex unitNormal = UnitNormal(edge, len);
 
+            // Get gauss quadrature points
             for (int g=0; g<gpe.size(); g++){gauss_p.at(g) = GaussMapPointsEdge({gpe[g]}, edge)};
 
             // Pick cell inside computational domain
             indice gcell_inside = PickCellInside(mi, gCellIn, gCellOut);
 
-            // Compute velocity of from computation results of stokes and darcy problems
+            // Compute velocity from computating results of stokes and darcy problems
             velocity_darcy = ExtractVelocity(sol_darcy, g_darcy, refmap_darcy, 
             mi, gauss_p, gcell_inside, hdiv, mybasis);
 
@@ -171,7 +182,7 @@ double * edgeFluxAll(const MeshInfo* mi,
             // Compute effective velocity 
 
             edgeflux[flatlocal] = edgeFlux(mi, mluIn, mluOut, gCellIn, gCellOut,
-            location(gCellIn), location(gCellOut), edge, gauss_p, velocity, gwe, {0.0},gluxfunc, fluxfuncbndry);
+            location(gCellIn), location(gCellOut), edge, gauss_p, velocity, gwe, {0.0},fluxfunc, fluxfuncbndry);
 
         }
     }
@@ -195,11 +206,46 @@ double * edgeFluxAll(const MeshInfo* mi,
             double len = length(edge);
             vertex unitNormal = UnitNormal(edge, len);
 
+            // Get gauss quadrature points
             for (int g=0; g<gpe.size(); g++){gauss_p.at(g) = GaussMapPointsEdge({gpe[g]}, edge);}
 
+            // Pick cell inside computational domain
+            indice gcell_inside = PickCellInside(mi, gCellIn, gCellOut);
+
+            // Compute velocity from computating results of stokes and darcy problems 
+            velocity_darcy = ExtractVelocity(sol_darcy, g_darcy, refmap_darcy, 
+            mi, gauss_p, gcell_inside, hdiv, mybasis);
+
+            velocity_stokes = ExtractVelocity(sol_stokes, g_stokes, refmap_stokes, 
+            mi, gauss_p, gcell_inside, br, mybasis);
+
+            edgeflux[mi.MPIlocalHoriEdgeSize + flatlocal] = edgeFlux(mi, mluIn, mluOut, gCellIn, gCellOut,
+            location(gCellIn), location(gCellOut), edge, gauss_p, velocity, gwe, {0.0},fluxfunc, fluxfuncbndry);
 
         }
     }
 
     return edgeflux;
+}
+
+double cellFlux(const indice& lCell, double * edgeFlux){
+
+    // Return summation of flux on the edges of a given cell
+
+    double flux = 0.0;
+
+    // Four edge index and corresponding local index
+    indice left   = lCell;
+    indice right  = lCell + {1,0};
+    indice bottom = lCell;
+    indice top    = lCell + {0,1};
+
+    int left_flat  = FlatIndic(mi.MPIlocalVertexSize[0],left);
+    int right_flat = FlatIndic(mi.MPIlocalVertexSize[0],left);
+    int bottom_flat= FlatIndic(mi.MPIlocalCellSize[0], bottom) + mi.localHoriEdgeSize;
+    int top_flat   = FlatIndic(mi.MPIlocalCellSize[0], top) + mi.localHoriEdgeSize;       
+
+    flux = edgeFlux[left_flat] + edgeFlux[right_flat] + edgeFlux[bottom_flat] + edgeFlux[top_flat]; 
+
+    return flux;
 }
