025bcf2ae52966cb28f4b555e9f20473b8794361
LuanBu <abandtian@gmail.com>
2025-01-07 16:12:35 -0600

    update

---
+++ src/mfem/test/1dTests/test.cpp
@@ -0,0 +1,33 @@
+#include <petsc.h>
+#include <iostream>
+#include <ctime>
+#include <chrono>
+#include "integral.h"
+#include "eutectic.h"
+#include "input.h"
+#include "util.h"
+
+// MFEM parameter header file
+#include "myFunc.h"
+
+//#define COUPLED
+#include "passemble.h"
+#include "Hdivmixed.h"
+#include "brmixed.h"
+#include "bndry.h"
+#include "preconst.h"
+#include "psolve.h"
+
+
+extern "C"{
+#include "mesh.h"
+#include "output.h"
+//#include "cgns_io.h"
+}
+
+int main(int argc, char ** argv){
+
+
+
+    return 0;
+}
---
+++ src/test1/run1dtest.sh
@@ -0,0 +1,5 @@
+cd build
+
+#./test -M 4 -N 80 -maxIter 30 -tol 1e-10 -tmax 0.1 -H 4.0 -ystart -2
+./test -M 4 -N 80 -maxIter 30 -tol 1e-10 -tmax 0.1 -H 0.2 -ystart -0.1
+
