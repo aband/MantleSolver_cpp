c17d992ebb9298783aca3a34d188ddff3993b1b8
LuanBu <abandtian@gmail.com>
2025-02-25 16:25:28 -0600

    update

---
+++ src/transport/diffusiveflux.cpp
@@ -0,0 +1,13 @@
+#include "diffusiveflux.h"
+
+double getfluxintegral(const MeshInfo& mi,
+                       const int& degree){
+
+    const valarray<double>& gwe = GaussWeightsEdge;
+    const valarray<double>& gpe = GaussPointsEdge;
+
+    double work = 0.0;
+
+
+    return work;
+}
---
+++ src/transport/diffusiveflux.h
@@ -0,0 +1,15 @@
+#ifndef DIFFUSIVEFLUX_H_
+#define DIFFUSIVEFLUX_H_
+
+#include "mluse.h"
+#include "trans_param.h"
+#include "lagrange_tmp.h"
+
+double edgefluxintegral(); 
+
+// Function used on boundary
+double edgefluxintegral();
+
+int edgefluxintegral();
+
+#endif
