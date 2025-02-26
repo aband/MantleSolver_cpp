1154cff16b0d40a0098db2748d923884cdc4daad
t <abandtian@gmail.com>
2025-02-25 23:44:16 -0600

    update

--- src/transport/diffusiveflux.cpp
+++ src/transport/diffusiveflux.cpp
@@ -1,12 +1,16 @@
 #include "diffusiveflux.h"
 
 double getfluxintegral(const MeshInfo& mi,
-                       const int& degree){
+                       const indice& gcellleft,
+                       const indice& gcellright){
 
     const valarray<double>& gwe = GaussWeightsEdge;
     const valarray<double>& gpe = GaussPointsEdge;
 
     double work = 0.0;
+    int degree = gwe.size() + 1;
+    const int numPts = std::ceil((degree+1)/2.0) * 2;
+
 
 
     return work;
