11502f016b87dce3c1ca75ad17eb4727e285807c
t <abandtian@gmail.com>
2025-04-09 00:28:16 -0500

    update

---
+++ src/driver/coupledsys_record/record.cpp
@@ -0,0 +1,14 @@
+int Driver::record_gauss_points(Tensor<vector<vertex>>& vert,
+                                Tensor<vector<vertex>>& hori){
+
+
+
+    return 1;
+}
+
+int Driver::record_phase(Tensor<vector<double>>& vert,
+                         Tensor<vector<double>>& hori){
+
+
+    return 1;
+}
