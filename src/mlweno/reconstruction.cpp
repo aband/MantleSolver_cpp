d5bd66819bdde7c5f5446c453f58618da989c4dc
LuanBu <abandtian@gmail.com>
2025-04-24 16:33:23 -0500

    update

--- src/mlweno/reconstruction.cpp
+++ src/mlweno/reconstruction.cpp
@@ -198,6 +198,7 @@ int multilevel::getsol(Tensor<double>& stencilsol, double ** localsol,
         localcellindexx = stencilindex[0] + i;
         localcellindexy = stencilindex[1] + j;
         stencilsol({i,j}) = localsol[localcellindexy][localcellindexx];
+		  cout << stencilsol({i,j}) << endl;
     }}
 
     return 1;
