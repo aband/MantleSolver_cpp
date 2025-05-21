6fa52300e4c76382a7f14d070a4d2ef083cc4025
aband <abandtian@gmail.com>
2025-05-21 01:10:16 -0500

    update

---
+++ src/driver/static/conv_test.cpp
@@ -0,0 +1,43 @@
+void AssignPhyProperties(PhysProperty * pp){
+
+    pp->theta = 0.0;
+    pp->mu_s  = 1e19;
+    pp->mu_f  = 1.0;
+    pp->rho_f = 2800;
+    pp->rho_s = 3300;
+    pp->gx    = 0.0;
+    pp->gy    = -10.0;
+    pp->invk0 = 1.0/(1e-8);
+    pp->phi0  = 0.4;
+    pp->U0    = 1e-9;
+    pp->L0    = 160*1000;
+    pp->V0    = 3.2/100/(365*24*60*60); //3.2 (cm/y)
+
+    // Non dimensionalization parameters
+
+    pp->rho_r = pp->rho_s - pp->rho_f;
+
+    pp->l0    = pow(pp->mu_s/pp->invk0/pp->mu_f,0.5);
+    pp->p0    = pp->gy*pp->l0*pp->rho_r;
+    pp->u0    = pp->gy*pp->rho_r/pp->mu_f/pp->invk0;
+
+    pp->l = 20/pp->l0;
+}
+
+double AssignPorosity(const vertex& point, PhysProperty * pp){
+
+    // used to identify incorrect porosity
+    return 0.2;
+}
+
+double AssignPorosity(double phi_f){
+    return 1-phi_f;
+}
+
+vertex bndryVs(const vertex& point, PhysProperty * pp){
+
+    // Stokes
+    vertex work {0.0,0.0};
+    double coef = 0.0;
+
+}
