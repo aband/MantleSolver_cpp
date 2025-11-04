bdcc621f2b2d5d681fe03ca276bf843f2932244c
aband <abandtian@gmail.com>
2025-11-03 23:46:53 -0600

    update

--- src/transport_array/diffusiveflux.cpp
+++ src/transport_array/diffusiveflux.cpp
@@ -2,6 +2,8 @@
 
 double edgefluxintegral(const reconstruction& recon_neg,
                         const reconstruction& recon_pos,
+								const indice& gcellin,
+								const indice& gcellout,
                         const vector<tensorstencilpoly>& sten_lg,
                         const vector<tensorstencilpoly>& sten_sm,
                         const int sampleSize, 
@@ -18,7 +20,30 @@ double edgefluxintegral(const reconstruction& recon_neg,
 
     int degree = gwe.size() + 1;
 
+    const int halfPts = std::ceil((degree+1)/2.0);
+    const int numPts =  halfPts * 2;
 
+    const double hIn  = mi.cellArea.at(FlatIndic(mi,gcellin));
+    const double hOut = mi.cellArea.at(FlatIndic(mi,gcellout));
+
+    const double h = 2.0 * ((hIn < hOut) ? hIn : hOut);
+
+    // Compute sample interval
+    const double dx = h /(double)(numPts - 1);
+
+    // Compute lagrange interpolation
+    LagrangeBasisDeriv lagDer(numPts - 1);
+
+    for (int g=0; g<gwe.size(); g++){
+        vertex mapped = GaussMapPointsEdge({gpe[g], edge});
+
+
+
+		  for (int i=0; i<numPts; i++){
+            work -= lagDer.middle(numPts-1, i)/dx * gwe[g] * len/2.0 * samples.at(i);
+		  } 
+
+    }
 
     return work;
 }
